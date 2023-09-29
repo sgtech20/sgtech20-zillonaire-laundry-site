7f4498d7f27bd5f949d33f12692f3d3d298a730b
Bolaji Oluwasegun <136334053+sgtech20@users.noreply.github.com>
2023-09-29 15:23:34 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,11 @@
 let navBtn = document.querySelector('.nav-btn');
-let closeBtn = document.querySelector('.close-nav-btn')
+let closeBtn = document.querySelector('.close-nav-btn');
 let mobileLinks = document.querySelector('.mobile-navLinks');
 
 navBtn.addEventListener('click', function(){
     mobileLinks.classList.add('show-links')
     navBtn.style.display = 'none';
     closeBtn.style.display = 'initial';
-
 })
 closeBtn.addEventListener('click', function(){
     mobileLinks.classList.remove('show-links')
