f82495b1226d6f33acc80c63f67f6cd5e55c79d0
Sg <bolajisegun2015@gmail.com>
2023-09-29 02:51:27 +0100

    finished up the mobile navBar and heroSection

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let navBtn = document.querySelector('.nav-btn');
+let closeBtn = document.querySelector('.close-nav-btn')
+let mobileLinks = document.querySelector('.mobile-navLinks');
+
+navBtn.addEventListener('click', function(){
+    mobileLinks.classList.add('show-links')
+    navBtn.style.display = 'none';
+    closeBtn.style.display = 'initial';
+
+})
+closeBtn.addEventListener('click', function(){
+    mobileLinks.classList.remove('show-links')
+    navBtn.style.display = 'initial';
+    closeBtn.style.display = 'none';
+})
